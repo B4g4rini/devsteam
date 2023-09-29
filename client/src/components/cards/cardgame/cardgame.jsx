a46bd6c5e3ed5f6206d941e3882298538dee25c3
B4g4rini <pedro.bagarini2000@gmail.com>
2023-09-29 15:47:42 -0300

    ajustados os textos dos cardgames e o carrinho

--- client/src/components/cards/cardgame/cardgame.jsx
+++ client/src/components/cards/cardgame/cardgame.jsx
@@ -3,36 +3,29 @@ import Button from "../../forms/button/button";
 
 import cs from "../../../assets/products/Counter Strike.png";
 
-
-
-
-
 const CardGame = () => {
   return (
     <div className={styles.CardGame}>
-          <div className={styles.game}>
-          <h2>Counter strike: Global offensive</h2>
-            <div className={styles.categorias}>
-            <h4>Ação, estratégia, multijogador.</h4>
-            </div>
-          </div>
-      <img
-        src={cs}
-        alt="Imagem do jogo CounterStrike"
-        width="270"
-        height="270"
-      />
-          <div className={styles.CardGame}>
-            <Button>Adicionar ao carrinho
-          <img src="/src/assets/cart-plus.svg" alt="icone de carrinho e compra" width="24" height="22" >
+      <div className={styles.game}>
+        <h2>Counter strike: Global offensive</h2>
+      <h1>99,90</h1>
+      </div>
+      <div className={styles.Categorias}>
+        <h3>Ação, estratégia, multijogador</h3>
 
-          </img>
+        <Button>
+          Adicionar ao carrinho
+          <img
+            src="/src/assets/cart-plus.svg"
+            alt="icone de carrinho e compra"
+            width="24"
+            height="22"
+          ></img>
         </Button>
-            </div>
-          </div>
-       
-    );
-  };
-;
-  
-  export default CardGame;
+      </div>
+
+      <img src={cs} alt="Imagem do jogo CounterStrike" />
+    </div>
+  );
+};
+export default CardGame;
